55733bc57c5ccb93c5ebcae4a441940ab2b4dfd5
mishradivyasish <130085421+mishradivyasish@users.noreply.github.com>
2025-09-17 01:12:24 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,42 +1,43 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 
-import App from './App.jsx';
-import './index.css';
-import Createtrip from './create-trip/index.jsx';
-import Header from './components/ui/custom/Header.jsx';
+import App from './App.jsx'
+import './index.css'
+import Createtrip from './create-trip/index.jsx'
+import Header from './components/ui/custom/Header.jsx'
 import { GoogleOAuthProvider } from '@react-oauth/google';
-import { Toaster } from 'sonner';
-import ViewTrip from './view-trip/[tripId]/index.jsx';
-import MyTrips from './my-trips/index.jsx';
 
+import { Toaster } from 'sonner'
+import ViewTrip from './view-trip/[tripId]/index.jsx'
+import MyTrips from './my-trips/index.jsx'
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <App />
+    element:<App/>
   },
   {
-    path: '/create-trip',
-    element: <Createtrip />
+    path:'/create-trip',
+    element:<Createtrip/>
+  },{
+    path:'/view-trip/:tripId',
+    element:<ViewTrip/>
   },
   {
-    path: '/view-trip/:tripId',
-    element: <ViewTrip />
-  },
-  {
-    path: '/my-trips',
-    element: <MyTrips />
-  }
-]);
+  path: "/my-trips",
+  element: <MyTrips />
+}
 
+])
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_AUTH_CLIENT_ID}>
-      <Header />
-      <Toaster />
-      <RouterProvider router={router} />
-    </GoogleOAuthProvider>
-  </StrictMode>
-);
+  <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_AUTH_CLIENT_ID}>
+  <Header/>
+  <Toaster/>
+  
+    <RouterProvider router={router}/>
+    </GoogleOAuthProvider>;
+  </StrictMode>,
+)
+
 
